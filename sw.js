03d33541474a92bcfb8921d74f52c63fdc69a33b
judyshm-coder <judy.s.hm@gmail.com>
2026-04-01 08:46:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
-const CACHE = 'wanderplan-v3';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'journi-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS.filter(a => !a.endsWith('.png') || true)))
+      .then(() => self.skipWaiting())
+  );
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
@@ -17,9 +23,11 @@ self.addEventListener('fetch', e => {
     return;
   }
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-      return res;
-    }))
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+    )
   );
 });
